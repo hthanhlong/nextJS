b18d514568764ba4ea8ea1385edbc95887c55dbb
hthanhlong97 <hthanhlong97@gmail.com>
2023-07-31 23:25:48 +0700

    update route

---
+++ app/about/[id]/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Page = ({params} : {
+    params : { id: string}
+}) => {
+    
+  return (
+    <div>{params.id}</div>
+  )
+}
+
+export default Page
--- app/contract/page.tsx
+++ app/contract/page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Contract = () => {
   return (
-    <div>Contract</div>
+    <div>Contract page</div>
   )
 }
 
